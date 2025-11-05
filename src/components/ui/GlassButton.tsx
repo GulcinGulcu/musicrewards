ecd31855c93e00ba94d080b25280f8163c0b6df1
gulcin <gulcin_375@hotmail.com>
2025-11-05 11:55:44 +0100

    feat: make GlassButton use dynamic theme colors

--- src/components/ui/GlassButton.tsx
+++ src/components/ui/GlassButton.tsx
@@ -1,51 +1,52 @@
-import { TouchableOpacity } from "react-native";
-import { LinearGradient } from "expo-linear-gradient";
-import { View, Text } from "react-native";
-import { StyleSheet } from "react-native";
-import React from "react";
+import React, { useMemo } from "react";
+import { TouchableOpacity, StyleSheet, ViewStyle } from "react-native";
+import { useThemeStore } from "../../stores/themeStore";
+import { ThemeMode } from "../../types";
 
-interface TransParentBgButton {
-  title?: string;
+interface GlassButtonProps {
   icon?: React.ReactNode;
+  title?: string | null;
   onPress: () => void;
+  style?: ViewStyle;
+  disabled?: boolean
 }
 
 export default function GlassButton({
-  title,
-  onPress,
   icon,
-}: TransParentBgButton) {
+  onPress,
+  style,
+  disabled
+}: GlassButtonProps) {
+  const { mode } = useThemeStore();
+  const styles = useMemo(() => getStyles(mode), [mode]);
+
   return (
-    <TouchableOpacity
-      activeOpacity={0.85}
-      onPress={onPress}
-      style={styles.button}
-    >
-      {icon && <View>{icon}</View>}
-      {title && <Text style={styles.text}>{title}</Text>}
+    <TouchableOpacity style={[styles.button, style]} onPress={onPress} activeOpacity={0.85} disabled={disabled}>
+      {icon}
     </TouchableOpacity>
   );
 }
 
-const styles = StyleSheet.create({
-  button: {
-    width: 50,
-    height: 50,
-    borderRadius: 30,
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: "rgba(255, 255, 255, 0.08)",
-    borderColor: "rgba(255, 255, 255, 0.15)",
-    shadowColor: "#000",
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.2,
-    shadowRadius: 4,
-    elevation: 4,
-  },
-  text: {
-    color: "#fff",
-    fontSize: 16,
-    fontWeight: "600",
-  },
-});
+const getStyles = (mode: ThemeMode) =>
+  StyleSheet.create({
+    button: {
+      borderRadius: 50,
+      padding: 12,
+      alignItems: "center",
+      justifyContent: "center",
+      backgroundColor:
+        mode === "dark"
+          ? "rgba(255, 255, 255, 0.08)" 
+          : "rgba(0, 0, 0, 0.05)",
+      borderColor:
+        mode === "dark"
+          ? "rgba(255, 255, 255, 0.15)"
+          : "rgba(0, 0, 0, 0.12)",
+      borderWidth: 1,
+      shadowColor: mode === "dark" ? "#000" : "#B0B0B0",
+      shadowOffset: { width: 0, height: 4 },
+      shadowOpacity: mode === "dark" ? 0.6 : 0.3,
+      shadowRadius: 8,
+      elevation: 6,
+    },
+  });
